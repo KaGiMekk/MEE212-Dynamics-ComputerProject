7939fe7b939107b0cc08cbe5700633ff55fdfa89
KaGi <kagisuwijak1@gmail.com>
2023-11-23 10:40:54 +0700

    fix dtheta_ddot equation

--- Module/Solve_equation.py
+++ Module/Solve_equation.py
@@ -3,7 +3,7 @@ from scipy.integrate import solve_ivp
 def equations_of_motion(t, y, T, m):
     R, R_dot, theta, theta_dot = y
     dR_ddot = R * theta_dot**2
-    dtheta_ddot = ((T / R) - (2 * m * R_dot * theta_dot)) / (m * R**2)
+    dtheta_ddot = ((T / R) - (2 * m * R_dot * theta_dot)) / (m * R)
     return [R_dot, dR_ddot, theta_dot, dtheta_ddot]
 
 def solve_equations(equations, t_span, y0, t_eval, T, m):
